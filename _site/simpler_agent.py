fa42b2e6e745bff4ba39558b8e8ab940ea94d1b6
Buck Shlegeris <bshlegeris@gmail.com>
2017-02-05 13:10:01 -0800

    initial commit

---
+++ _site/simpler_agent.py
@@ -0,0 +1,40 @@
+def SimpleAttentionSchemaAgent:
+  def __init__(self):
+    self.visual_system = VisualSystem()
+    self.model = Model()
+    self.planner = Planner()
+
+  def think(self, visual_input):
+    """
+    This method contains all the details of how information flows between
+    different components of the agent. Details of how information is
+    processed inside each component of the agent are specified in the
+    classes for each component.
+    """
+    # Process world
+    self.visual_system.update(visual_input)
+
+    # Update world schema with visual information from the visual system.
+    self.model.world_schema.update_with_visual_info(self.visual_system.visual_info())
+
+    # Update attention schema with the information about visual attention
+    # that the visual system can provide
+    self.model.attention_schema.update_with_visual_attention_info(
+      self.visual_system.visual_attention_schema_info())
+
+    # Plan action. The planner uses the information available to it from
+    # the visual system directly and from its world schema and attention schema.
+    self.plan = self.planner.plan({
+      "world_schema": self.model.world_schema.planner_info(),
+      "attention_schema": self.model.attention_schema.planner_info()
+    })
+
+    # Update our model of what our planner was mostly thinking about
+    self.model.attention_schema.update_with_planner_attention_info(
+      self.planner.planner_attention_schema_info)
+
+    # The planning system gets to control the attention of the visual
+    # system (top-down attention)
+    self.visual_system.control_attention(self.plan.new_visual_attention)
+
+    return self.plan.action
---
+++ simple_agent.py
@@ -0,0 +1,27 @@
+class Agent:
+    def __init__(self):
+        self.vision_system = VisionSystem()
+        self.model = Model()
+        self.planning_system = PlanningSystem()
+
+        self.model.attach_visual_system(self.vision_system)
+
+        self.planning_system.attach_visual_system(self.vision_system)
+        self.planning_system.attach_model(self.model)
+
+    def step(self, visual_input):
+        self.vision_system.handle_input(visual_input)
+
+        self.model.update()
+
+        self.planning_system.get_action()
+
+class Model:
+    def __init___(self):
+        self.attention_schema = {}
+        self.world_schema = {}
+        self.self_schema = {}
+
+    def update(self):
+        pass
+
